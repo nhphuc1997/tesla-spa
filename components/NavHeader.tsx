576123f2239afba77be984d4f74d06623241b973
phucnh17 <phucnh17@msb.com.vn>
2024-07-01 09:13:58 +0700

    feat: init

--- components/NavHeader.tsx
+++ components/NavHeader.tsx
@@ -1,26 +1,22 @@
 'use client'
-import { DoubleRightOutlined, SearchOutlined } from "@ant-design/icons";
+import { DoubleRightOutlined, MenuOutlined, SearchOutlined } from "@ant-design/icons";
 import { Header } from "antd/es/layout/layout";
 import { Col, Row, Space, Typography } from 'antd';
 
 const NavHeader = () => {
   return (
-    <Header className="px-3 sticky top-0 w-full flex items-center bg-white">
+    <Header className="px-3 sticky top-0 w-full flex items-center bg-white z-10">
       <Row className="w-full">
-        <Col span={6}>
-          <div className="flex justify-start items-center">
-            <DoubleRightOutlined />
-          </div>
+        <Col span={6} className="flex justify-start items-center">
+          <MenuOutlined />
         </Col>
         <Col span={12}>
           <Typography.Title level={5} className="h-full text-center flex justify-center items-center !mb-0 text-sm md:text-md">
             SuperCar BlonDie
           </Typography.Title>
         </Col>
-        <Col span={6}>
-          <div className="flex justify-end items-center">
-            <SearchOutlined />
-          </div>
+        <Col span={6} className="flex justify-end items-center">
+          <SearchOutlined />
         </Col>
       </Row>
     </Header>
--- components/ProductsCard.tsx
+++ components/ProductsCard.tsx
@@ -1,8 +1,61 @@
+import { CheckCircleOutlined, EyeOutlined, ImportOutlined, InfoCircleOutlined, InfoOutlined, PushpinOutlined, TagsOutlined } from "@ant-design/icons";
+import { Col, Row, Tag, Tooltip } from "antd";
+import { Image, Rate, Typography } from 'antd';
+
 const ProductsCard = () => {
   return (
-    <div className="h-full w-full border-r-[1px] border-l-[1px] p-2">
-1
-    </div>
+    <Row className="h-full w-full border-r-[1px] border-l-[1px] p-2">
+      {
+        [1, 1, 2, 3, 4, 1].map(item => (
+          <Col xs={24} sm={12} md={4} key={item} className="p-1">
+            <div className="p-2 rounded-sm border hover:drop-shadow-2xl cursor-pointer">
+              <div className="">
+                <Image className="relative rounded-lg" src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png" />
+                <InfoCircleOutlined className="absolute top-4 right-4 text-xs text-white cursor-pointer" />
+              </div>
+              <div className="">
+                <div className="flex justify-start items-center">
+                  <Tag className="bg-[rgba(41,41,41,.75)] text-white">
+                    1963
+                  </Tag>
+                  <Typography.Paragraph className="!mb-0">
+                    <Tag icon={<ImportOutlined />} className="bg-[rgba(41,41,41,.75)] text-white">
+                      Gemany
+                    </Tag>
+                  </Typography.Paragraph>
+                  <Typography.Paragraph className="!mb-0">
+                    <Tag icon={<TagsOutlined />} className="bg-[rgba(41,41,41,.75)] text-white">
+                      5,000$
+                    </Tag>
+                  </Typography.Paragraph>
+                </div>
+
+                <div className="flex justify-between items-center">
+                  <Tooltip title="prompt text">
+                    <Typography.Title level={4} className="!my-0 truncate">
+                      Aston Martin
+                    </Typography.Title>
+                  </Tooltip>
+
+                  <Typography.Paragraph className="!mb-0">
+                    12 <EyeOutlined className="text-xs px-1" />
+                  </Typography.Paragraph>
+                </div>
+
+                <div>
+                  <Tooltip title="prompt text">
+                    <Typography.Paragraph className="!mb-0 truncate">
+                      <PushpinOutlined /> DB4 Series V Vantage
+                    </Typography.Paragraph>
+                  </Tooltip>
+                </div>
+
+              </div>
+            </div>
+          </Col>
+        ))
+      }
+    </Row>
   )
 }
 
