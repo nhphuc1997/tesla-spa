93348dfdefb540e074c62b89b39e2f2ab60c906e
nhphuc1997 <nguyen.hong.phuc20211997@gmail.com>
2024-07-03 00:49:43 +0700

    feat: animation

--- components/ExpBuyCar.tsx
+++ components/ExpBuyCar.tsx
@@ -1,6 +1,7 @@
 import { Col, Divider, Row, Typography, Tag, Tooltip } from "antd"
 import mc1 from '@/assets/images/mc1.jpg'
 import Image from "next/image"
+import { CalendarOutlined } from "@ant-design/icons"
 
 const ExpBuyCar = () => {
   return (
@@ -21,13 +22,13 @@ const ExpBuyCar = () => {
         }
       </Col>
 
-      {[1, 2, 3, 4, 5, 6].map(item => (
-        <Col span={6} className="hover:shadow-md rounded-lg">
-          <div className="w-full flex justify-start items-start p-1 m-2">
-            <div className="w-1/2">
-              <Image className="rounded-lg" src={mc1} alt="" />
+      {[1, 2,3,4,5,6,7,8].map(item => (
+        <Col xs={12} md={6} className="hover:shadow-md rounded-lg" key={item}>
+          <div className="w-full flex justify-start items-start p-3 flex-col md:flex-row">
+            <div className="w-full md:w-1/2 md:pr-2">
+              <Image className="flex justify-center items-center rounded-lg" src={mc1} alt="" />
             </div>
-            <div className="w-1/2 px-2">
+            <div className="w-full md:w-1/2">
               <Typography.Title level={5} className="!mb-0 truncate">
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry
               </Typography.Title>
@@ -36,10 +37,17 @@ const ExpBuyCar = () => {
                 when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                 It has survived not only five centuries,
                 but also the leap into electronic typesetting">
-                <Typography.Paragraph className="truncate">
-                  Lorem Ipsum has been the industry's standard
+                <Typography.Paragraph className="!mb-0 truncate">
+                  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                  when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                  It has survived not only five centuries,
+                  but also the leap into electronic typesetting
                 </Typography.Paragraph>
               </Tooltip>
+
+              <Typography.Text className="truncate">
+                <CalendarOutlined /> 20/12/2024
+              </Typography.Text>
             </div>
           </div>
         </Col>
