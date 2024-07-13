d2afa30f5dd8970bf5e6fd1913f225bb6ea25718
phucnh17 <phucnh17@msb.com.vn>
2024-07-14 01:20:14 +0700

    feat: change base url

--- seed.sh
+++ seed.sh
@@ -83,4 +83,55 @@ curl -X 'POST' \
   ]
 }'
 
+curl -X 'POST' \
+  'http://13.213.88.50/backend/categories/bulk' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "bulk": [
+    {
+      "name": "Mercedes-Benz"
+    },
+    {
+      "name": "lexus"
+    },
+    {
+      "name": "Audi"
+    },
+    {
+      "name": "Bentley"
+    },
+    {
+      "name": "BMW"
+    }
+  ]
+}'
+
+
+curl -X 'POST' \
+  'http://13.213.88.50/backend/color-groups/bulk' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "bulk": [
+    {
+      "name": "white"
+    },
+    {
+      "name": "Red"
+    },
+    {
+      "name": "Grey"
+    },
+    {
+      "name": "Blue"
+    },
+    {
+      "name": "Titan"
+    },
+    {
+      "name": "Black"
+    }
+  ]
+}'
 
