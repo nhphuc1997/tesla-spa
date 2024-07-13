a61dcf135d7674a534ef6d5a3e10cd93553193c5
phucnh17 <phucnh17@msb.com.vn>
2024-07-13 15:53:01 +0700

    feat: change base url

--- components/detail-product/OrderView.tsx
+++ components/detail-product/OrderView.tsx
@@ -1,9 +1,11 @@
 import { formatCurrency } from "@/utils/format-currency";
 import {
+  Affix,
   Button,
   Descriptions,
   Divider,
   Drawer,
+  Modal,
   QRCode,
   Result,
   Steps,
@@ -18,43 +20,65 @@ import Slicker from "../Slicker";
 
 export default function OrderView() {
   const [openDrawOrder, setOpenDrawOrder] = useState(false);
+  const [openModalBookATestDrive, setOpenModalBookATestDrive] = useState(false);
   const [personalInfor, setOpersonalInfor] = useState(false);
   const [currentStepOrder, setCurrentStepOrder] = useState<number>(0);
 
   return (
-    <div>
-      <div className="p-3 border">
-        <Typography.Title level={5} className="!mt-3 text-center">
-          Mescedes benz
-        </Typography.Title>
-        <Divider className="" />
-        <div>
-          <Descriptions title="" column={1}>
-            <Descriptions.Item label="Material">Material</Descriptions.Item>
-            <Descriptions.Item label="Exterior">Exterior</Descriptions.Item>
-            <Descriptions.Item label="Interior">Exterior</Descriptions.Item>
-            <Descriptions.Item label="Alloys">Alloys</Descriptions.Item>
-            <Descriptions.Item label="Description">
-              Description
-            </Descriptions.Item>
-          </Descriptions>
-        </div>
-        <Divider className="" />
-        <div>
-          <Typography.Title level={5} className="text-right">
-            TOTAL: {formatCurrency(100000)}
+    <Affix offsetTop={76}>
+      <div className="">
+        <div className="p-3 border">
+          <Typography.Title level={5} className="!mt-3 text-center">
+            Mescedes benz
           </Typography.Title>
+          <Divider className="" />
+          <div>
+            <Descriptions title="" column={1}>
+              <Descriptions.Item label="Material">Material</Descriptions.Item>
+              <Descriptions.Item label="Exterior">Exterior</Descriptions.Item>
+              <Descriptions.Item label="Interior">Exterior</Descriptions.Item>
+              <Descriptions.Item label="Alloys">Alloys</Descriptions.Item>
+              <Descriptions.Item label="Description">
+                Description
+              </Descriptions.Item>
+            </Descriptions>
+          </div>
+          <Divider className="" />
+          <div>
+            <Typography.Title level={5} className="text-right">
+              TOTAL: {formatCurrency(100000)}
+            </Typography.Title>
+          </div>
+
+          <div>
+            <Button
+              className="!bg-black !text-white"
+              block
+              onClick={() => setOpenDrawOrder(true)}
+            >
+              Order
+            </Button>
+          </div>
+
+          <div>
+            <Button
+              className="!bg-black !text-white"
+              block
+              onClick={() => setOpenModalBookATestDrive(true)}
+            >
+              Book A Test Drive
+            </Button>
+          </div>
         </div>
 
-        <div>
-          <Button
-            className="!bg-black !text-white"
-            block
-            onClick={() => setOpenDrawOrder(true)}
-          >
-            Order
-          </Button>
-        </div>
+        <Modal
+          title="Book A Test Drive"
+          open={openModalBookATestDrive}
+          onCancel={() => setOpenModalBookATestDrive(false)}
+          footer={null}
+        >
+          <BookATestDrive />
+        </Modal>
 
         <Drawer
           className=""
@@ -136,16 +160,6 @@ export default function OrderView() {
           </div>
         </Drawer>
       </div>
-
-      <div className="mt-3">
-        <div className="p-3 border">
-          <Typography.Title level={5} className="!mt-3 text-center">
-            Book a test Drive
-          </Typography.Title>
-          <Divider className="" />
-          <BookATestDrive />
-        </div>
-      </div>
-    </div>
+    </Affix>
   );
 }
