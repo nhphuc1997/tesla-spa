fb713f7d30e37c0f255f21808c0d6a4f4f5b551e
nhphuc1997 <nguyen.hong.phuc20211997@gmail.com>
2024-07-13 14:04:08 +0700

    feat: change layout

--- components/detail-product/BookATestDrive.tsx
+++ components/detail-product/BookATestDrive.tsx
@@ -1,11 +1,17 @@
 'use client'
 import { useState } from "react";
-import FormStep1 from "./FormStep1";
-
+import FormStep1 from "./forms/FormStep1";
+import FormStep2 from "./forms/FormStep2";
+import { Result, Typography } from "antd";
 
 export default function BookATestDrive() {
   const [currentStep, setCurrentStep] = useState(0)
   const [valueFormStep1, setValueFormStep1] = useState(null)
+  const [valueFormStep2, setValueFormStep2] = useState(null)
+
+  console.log(valueFormStep1, 'valueFormStep1');
+  console.log(valueFormStep2, 'valueFormStep2');
+
 
   return (
     <div className="">
@@ -13,6 +19,31 @@ export default function BookATestDrive() {
         <FormStep1
           setCurrentStep={setCurrentStep}
           setValueFormStep1={setValueFormStep1}
+        />
+      )}
+
+      {currentStep === 1 && (
+        <FormStep2
+          setCurrentStep={setCurrentStep}
+          setValueFormStep2={setValueFormStep2}
+        />
+      )}
+
+      {currentStep === 2 && (
+        <Result
+          status="success"
+          title={
+            <div>
+              <Typography.Title>
+                Book a Test Drive successfully
+              </Typography.Title>
+
+              <Typography.Text>
+                we will contact you as soon as possible!!
+              </Typography.Text>
+            </div>
+          }
+          subTitle=""
         />
       )}
     </div>
---
+++ components/detail-product/forms/FormStep2.tsx
@@ -0,0 +1,78 @@
+'use client'
+import { DISTANCE_RADIUS } from "@/utils";
+import { SendOutlined, UserOutlined } from "@ant-design/icons";
+import { Button, DatePicker, Form, FormProps, Input, Select } from "antd";
+import TextArea from "antd/es/input/TextArea";
+import { useState } from "react"
+
+type FieldType = {
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+  contactNumber?: string;
+  additionalInfor?: string;
+};
+
+interface Props {
+  name?: string,
+  setCurrentStep: any,
+  setValueFormStep2: any,
+}
+
+export default function FormStep2({ setCurrentStep, setValueFormStep2 }: Props) {
+  const onFinishStep2: FormProps<FieldType>['onFinish'] = (values) => {
+    setValueFormStep2(values);
+    setCurrentStep(2)
+  }
+
+  return (
+    <Form name="basic" autoComplete="off" onFinish={onFinishStep2}>
+      <div className="pb-2">
+        <Form.Item<FieldType> name="firstName" rules={[{ required: true }]}>
+          <Input
+            onChange={e => console.log(e)}
+            placeholder="Enter your first name"
+            prefix={<UserOutlined />}
+          />
+        </Form.Item>
+      </div>
+
+      <div className="pb-2">
+        <Form.Item<FieldType> name="lastName" rules={[{ required: true }]}>
+          <Input
+            placeholder="Enter your last name"
+            prefix={<UserOutlined />}
+          />
+        </Form.Item>
+      </div>
+
+      <div className="pb-2">
+        <Form.Item<FieldType> name="email" rules={[{ required: true }]}>
+          <Input
+            placeholder="Enter your email"
+            prefix={<UserOutlined />}
+          />
+        </Form.Item>
+      </div>
+
+      <div className="pb-2">
+        <Form.Item<FieldType> name="contactNumber" rules={[{ required: true }]}>
+          <Input
+            placeholder="Enter your contact number"
+            prefix={<UserOutlined />}
+          />
+        </Form.Item>
+      </div>
+
+      <div className="pb-2">
+        <Form.Item<FieldType> name="additionalInfor" rules={[{ required: false }]}>
+          <TextArea placeholder="additional information" allowClear />
+        </Form.Item>
+      </div>
+
+      <div className="flex justify-end items-center">
+        <Button iconPosition={"end"} icon={<SendOutlined />} htmlType="submit">Next</Button>
+      </div>
+    </Form>
+  )
+}
