cb2ad8268f6f2864fa398821231f1c2fc3e4246d
phucnh17 <phucnh17@msb.com.vn>
2024-07-14 23:47:46 +0700

    feat: change base url

--- app/orders-history/page.tsx
+++ app/orders-history/page.tsx
@@ -1,11 +1,147 @@
 "use client";
 import { doGet } from "@/utils/doMethod";
-import { ORDER_HISTORY_COLUMNS } from "@/utils/order-history.colums";
+import { formatCurrency } from "@/utils/format-currency";
+import { formatDate } from "@/utils/format-date";
 import { useUser } from "@clerk/nextjs";
 import { useQuery } from "@tanstack/react-query";
-import { Col, Input, Row, Select, Spin, Table } from "antd";
+import {
+  Col,
+  Descriptions,
+  Input,
+  Row,
+  Select,
+  Spin,
+  Table,
+  TableProps,
+  Tag,
+  Tooltip,
+  Typography,
+} from "antd";
 import { useParams } from "next/navigation";
 import { useState } from "react";
+
+interface DataType {
+  key: string;
+  orderId: string;
+  amount: number;
+  interior: Record<string, any>;
+  exterior: Record<string, any>;
+  alloy: Record<string, any>;
+  product: Record<string, any>;
+  createAt: string;
+  type: string;
+}
+
+const ORDER_HISTORY_COLUMNS: TableProps<DataType>["columns"] = [
+  {
+    key: "orderId",
+    dataIndex: "orderId",
+    title: "order ID",
+    width: 100,
+    render: (text: string) => (
+      <Tooltip title={text}>
+        <Typography.Text ellipsis={true} className="w w-24">
+          {text}
+        </Typography.Text>
+      </Tooltip>
+    ),
+  },
+  {
+    key: "amount",
+    dataIndex: "amount",
+    title: "Amount",
+    width: 100,
+    render: (amount: number) => (
+      <Typography.Text className="truncate">
+        {formatCurrency(amount)}
+      </Typography.Text>
+    ),
+  },
+  {
+    key: "interior",
+    dataIndex: "interior",
+    title: "Interior",
+    render: (element: any) => (
+      <Descriptions column={1} className="!p-0 !m-0">
+        <Descriptions.Item label="Name" className="!p-0 truncate">
+          <Tooltip placement="topLeft" title={element?.name}>
+            {element?.name}
+          </Tooltip>
+        </Descriptions.Item>
+        <Descriptions.Item label="Cost">
+          {formatCurrency(element?.price)}
+        </Descriptions.Item>
+      </Descriptions>
+    ),
+  },
+  {
+    key: "exterior",
+    dataIndex: "exterior",
+    title: "Exterior",
+    render: (element: any) => (
+      <Descriptions column={1} className="!p-0 !m-0">
+        <Descriptions.Item label="Name" className="!p-0 truncate">
+          <Tooltip placement="topLeft" title={element?.name}>
+            {element?.name}
+          </Tooltip>
+        </Descriptions.Item>
+        <Descriptions.Item label="Cost">
+          {formatCurrency(element?.price)}
+        </Descriptions.Item>
+      </Descriptions>
+    ),
+  },
+  {
+    key: "alloy",
+    dataIndex: "alloy",
+    title: "Alloy",
+    render: (element: any) => (
+      <Descriptions column={1} className="!p-0 !m-0">
+        <Descriptions.Item label="Name" className="!p-0 truncate">
+          <Tooltip placement="topLeft" title={element?.name}>
+            {element?.name}
+          </Tooltip>
+        </Descriptions.Item>
+        <Descriptions.Item label="Cost">
+          {formatCurrency(element?.price)}
+        </Descriptions.Item>
+      </Descriptions>
+    ),
+  },
+  {
+    key: "product",
+    dataIndex: "product",
+    title: "Product",
+    render: (element: any) => (
+      <Descriptions column={1} className="!p-0 !m-0">
+        <Descriptions.Item label="Name" className="!p-0 truncate">
+          <Tooltip placement="topLeft" title={element?.name}>
+            {element?.name}
+          </Tooltip>
+        </Descriptions.Item>
+        <Descriptions.Item label="Cost" className="!p-0">
+          {formatCurrency(element?.price)}
+        </Descriptions.Item>
+        <Descriptions.Item label="Type">
+          <Tag className="!bg-black !text-white">{element?.kind}</Tag>
+        </Descriptions.Item>
+      </Descriptions>
+    ),
+  },
+  {
+    key: "createAt",
+    dataIndex: "createAt",
+    title: "Create At",
+    width: 100,
+    render: (text: string) => (
+      <Tooltip title={text}>
+        <Typography.Text ellipsis={true} className="w w-24">
+          {formatDate(text)}
+        </Typography.Text>
+      </Tooltip>
+    ),
+  },
+];
 
 export default function OrderHistory() {
   const { user } = useUser();
@@ -59,7 +195,7 @@ export default function OrderHistory() {
           </Col>
         </Row>
 
-        <Row>
+        <Row className="">
           <Col span={24}>
             <Table
               loading={loading}
---
+++ utils/format-date.ts
@@ -0,0 +1,4 @@
+export const formatDate = (date: string) => {
+  if (!date) return '--'
+  return new Date(date).toLocaleDateString()
+}
